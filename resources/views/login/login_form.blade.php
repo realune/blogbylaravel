b9a696e4410489bec31b73a51e01748ec5ed93b9
realune <peiu.1220@gmail.com>
2022-06-29 21:42:22 +0100

    implemented login validation

--- resources/views/login/login_form.blade.php
+++ resources/views/login/login_form.blade.php
@@ -14,23 +14,29 @@
   <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class="text-center">
-  <main class="form-signin w-100 m-auto" method="POST" action="{{ route('login') }}">
-    <form>
-      <h1 class="h3 mb-3 fw-normal">Login Form</h1>
-
-      <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
-        <label for="floatingInput">Email</label>
-      </div>
-      <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
-        <label for="floatingPassword">Password</label>
-      </div>
-
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-    </form>
-  </main>
+<body>
+  <form class="form-signin w-100 m-auto" method="POST" action="{{ route('login') }}">
+    @csrf
+    <h1 class="h3 mb-3 fw-normal">Login Form</h1>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+    @endif
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
+      <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+  </form>
 </body>
 
 </html>
